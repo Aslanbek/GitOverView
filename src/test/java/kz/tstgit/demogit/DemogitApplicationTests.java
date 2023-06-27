e5627d1e9e24471ea213ce0e487656323a96d505
Aslanbek Kulantayev <a.kulantayev@gmail.com>
2023-06-27 23:58:21 +0600

    init commit

---
+++ src/test/java/kz/tstgit/demogit/DemogitApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.tstgit.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
